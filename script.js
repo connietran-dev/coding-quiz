ada27d6b7a189291b5f15f022def66e75eb58043
Connie Tran <connietrandev@gmail.com>
2020-03-23 23:01:24 -0400

    Add comments. Troubleshoot event listener on quiz choices

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-// When "Start Quiz" button is clicked, create the below HTML elements.
-// TODO: And use data-attributes to set default elements to blank so they no longer show
-
-
 var body = document.body;
 
 // Create mainDiv to center content
 var mainDiv = document.createElement("div");
 
+// Create elements for quiz
 var quizQuestion = document.createElement("h1");
 var quizChoices = document.createElement("div");
 var startQuiz = document.createElement("button");
 
+// Set default text upon first load
 quizQuestion.textContent = "Coding Quiz Challenge";
 quizChoices.innerHTML = "<p>Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!</p>";
 startQuiz.textContent = "Start Quiz";
 
+// Append to document for display
 body.appendChild(mainDiv);
 
 mainDiv.appendChild(quizQuestion);
@@ -22,6 +21,7 @@ mainDiv.appendChild(quizChoices);
 mainDiv.appendChild(startQuiz);
 
 
+// When "Start Quiz" button is clicked, runFirstQuestion and hide button
 var startQuiz = document.querySelector("button");
 
 startQuiz.addEventListener("click", function() {
@@ -30,36 +30,26 @@ startQuiz.addEventListener("click", function() {
 });
 
 
+// Display first question
 
 function runFirstQuestion() {
 
 // Set the text content of trivia question and choices
 quizQuestion.textContent = "Commonly used data types DO NOT include:";
-quizChoices.innerHTML = "<ol><li>strings</li><li>booleans</li><li>alerts</li><li>numbers</li></ol>";
+quizChoices.innerHTML = "<li>strings</li><li>booleans</li><li>alerts</li><li>numbers</li>";
 
 };
+
+
+// runSecondQuestion() on click of li
+
 
 
 
 function runSecondQuestion() {
 
-// Set the body to a variable
-var body = document.body;
-
-// Create div containers to center content
-var mainDiv = document.createElement("div");
-
-// Create div containers for trivia question
-var quizQuestion = document.createElement("div");
-var quizChoices = document.createElement("div");
-
 // Set the text content of trivia question and choices
 quizQuestion.textContent = "The condition in an if / else statement is enclosed within _________";
-quizChoices.innerHTML = "hello";
-
-// Append elements to document for display
-body.appendChild(mainDiv);
-mainDiv.appendChild(quizQuestion);
-mainDiv.appendChild(quizChoices);
+quizChoices.innerHTML = "<li>quotes</li><li>curly brackets</li><li>parentheses</li><li>square brackets</li>";
 
 };
